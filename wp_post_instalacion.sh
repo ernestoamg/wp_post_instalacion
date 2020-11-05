0748006ff238dc4e4a36dcc0a57700c0b3c57056
ernestoamg <ernestoamg@live.com>
2020-11-05 11:19:37 -0500

    Update wp_post_instalacion.sh

--- wp_post_instalacion.sh
+++ wp_post_instalacion.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #wp_post_instalacion.sh
+# http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+red=`tput setaf 1`;
+yellow=`tput setaf 3`;
+green=`tput setaf 2`;
+clear=`tput sgr0`;
+
+#desproteger el wp-config.php para realizar cambios
+chmod 644 wp-config.php
 
 clear
 
@@ -48,7 +56,7 @@ wp plugin install mainwp-child-reports --activate $allowroot
 wp plugin install wp-fastest-cache --activate $allowroot
 wp plugin install wp-reset --activate $allowroot
 
-read -p "Instalar los plugins de seguridad PRO? [s/n]: " instalar_securitypro
+read -p "${yellow}Instalar los plugins de seguridad PRO? [s/n]:${clear} " instalar_securitypro
 if [ "$instalar_securitypro" == s ] ; then
 	wp plugin install https://undominio.com/unpluginespecial.zip $allowroot
 else
@@ -57,13 +65,13 @@ else
 	echo "Versión gratuita instalada."
 fi
 
-read -p "Instalar editor clásico? [s/n]: " instalar_editorclasico
+read -p "${yellow}Instalar editor clásico? [s/n]: ${clear}" instalar_editorclasico
 if [ "$instalar_editorclasico" == s ] ; then
 	wp plugin install classic-editor $allowroot
 fi
 
 #sección especial para instalar plugins de migracion
-read -p "Instalar plugins de migración PRO? [s/n]: " instalar_migracion
+read -p "${yellow}Instalar plugins de migración PRO? [s/n]: ${clear}" instalar_migracion
 if [ "$instalar_migracion" == s ] ; then
 	wp plugin install https://undominio.com/unpluginespecial.zip $allowroot
 	wp plugin install https://undominio.com/unpluginespecial.zip $allowroot
@@ -73,9 +81,47 @@ else
 	echo "Versión gratuita instalada."
 fi
 
-clear
+echo "============================================================"
+echo "ATENCIÓN: UTILICE ESTA SECCIÓN BAJO SU PROPIO RIESGO."
+echo "Renombrar el prefijo de las tablas de la base de datos."
+echo "Si algo sale mal, se puede dañar su sitio web."
+echo "Antes de responder 's' asegúrese de terner backup del wp-config.php y su base de datos."
+echo "============================================================"
+echo ""
+echo "Estas son sus tablas actuales:"
+wp db tables
+echo ""
+#sección para renombrar cambiar el prefijo de las tablas de la base de datos
+read -p "${yellow}¿Desea renombrar los prefijos de las tablas? [s/n]: ${clear}" renombrar_prefijos
+if [ "$renombrar_prefijos" == s ] ; then
+	echo "Instalando paquete..."
+	wp package install iandunn/wp-cli-rename-db-prefix $allowroot
+	echo ""
+	echo "Se le preguntará si se procede con la operación:"
+	read -p "Nuevo prefijo para sus tablas: " nuevo_prefijo
+	wp rename-db-prefix $nuevo_prefijo $allowroot
+else
+	echo "Ok, saltando cambio de prefijo."
+fi
+echo "Tablas renombradas:"
+wp db tables
+echo ""
+read -n 1 -r -s -p $'${yellow}Verifique y presione una tecla para continuar o CTRL+C para cancelar...\n${clear}'
 
-read -p "Deseas crear un .htaccess default? [s/n]: " instalar_htaccess
+clear
+echo "============================================================"
+echo "Opciones adicionales de seguridad"
+echo "Las siguientes opciones agregan código al archivo .htaccess"
+echo "Si ya ejecutó estas tareas, no las vuelva a ejecutar"
+echo "============================================================"
+echo ""
+read -n 1 -r -s -p $'Presione una tecla para continuar o CTRL+C para cancelar...\n'
+echo ""
+
+echo "Borrando archivos: readme.html license.txt wp-config-sample.php..."
+rm -rf readme.html license.txt wp-config-sample.php;
+
+read -p "${yellow}¿Deseas crear un .htaccess default? [s/n]: ${clear}" instalar_htaccess
 if [ "$instalar_htaccess" == s ] ; then
 echo "Creando archivo .htaccess"
 cat > .htaccess<<EOF
@@ -93,24 +139,12 @@ else
 	echo "Ok, saltando .htaccess nuevo."
 fi
 
-echo "============================================================"
-echo "Opciones adicionales de seguridad"
-echo "Las siguientes opciones agregan código al archivo .htaccess"
-echo "Si ya ejecutó estas tareas, no las vuelva a ejecutar"
-echo "============================================================"
-echo ""
-read -n 1 -r -s -p $'Presione una tecla para iniciar o CTRL+C para cancelar...\n'
-echo ""
-
-echo "Borrando archivos: readme.html license.txt wp-config-sample.php..."
-rm -rf readme.html license.txt wp-config-sample.php;
-
-read -p "Deseas ver el archivo .htaccess? [s/n]: " ver_htaccess
+read -p "${yellow}¿Deseas ver el archivo .htaccess? [s/n]: ${clear}" ver_htaccess
 if [ "$ver_htaccess" == s ] ; then
 	cat .htaccess
 fi
 
-read -p "Deseas bloquear el acceso al wp-config.php? [s/n]: " bloquear_wp_config
+read -p "${yellow}¿Deseas bloquear el acceso al wp-config.php? [s/n]: ${clear}" bloquear_wp_config
 if [ "$bloquear_wp_config" == s ] ; then
 	echo "Bloqueando..."
 	echo "
@@ -123,10 +157,14 @@ else
 	echo "Ok, saltando bloqueo del wp-config.php."
 fi
 
-read -p "Deseas proteger la carpeta uploads? [s/n]: " proteger_uploads
+read -p "${yellow}¿Deseas proteger la carpeta uploads? [s/n]: ${clear}" proteger_uploads
 if [ "$proteger_uploads" == s ] ; then
-	echo "protegiendo..."
-	echo '
+
+	ht_file="wp-content/uploads/.htaccess"
+	if [ ! -f "$ht_file" ]
+
+		echo "protegiendo..."
+		echo '
 <Files ~ ".*\..*">
 	Order Allow,Deny
 	Deny from all
@@ -134,14 +172,38 @@ if [ "$proteger_uploads" == s ] ; then
 <FilesMatch "\.(jpg|jpeg|jpe|gif|png|bmp|tif|tiff|doc|pdf|rtf|xls|numbers|odt|pages|key|mp3|mp4|webm|flv|webp)$">
 	Order Deny,Allow
 	Allow from all
-</FilesMatch>' >> .htaccess
-	echo "Ok, carpeta uploads protegida."
+</FilesMatch>' >> wp-content/uploads/.htaccess
+		echo "Ok, carpeta uploads protegida."
+	then
+	    echo "${red} $0: El archivo'${ht_file}' ya existe, no se hicieron cambios.${clear}"
+	fi
 else
 	echo "Ok, saltando protección de carpeta uploads."
 fi
 
-read -p "Deseas bloquear los pingbacks? [s/n]: " bloquear_pingbacks
+read -p "${yellow}¿Deshabilita la ejecución de archivos PHP en uploads? [s/n]: ${clear}" deshablita_php
+if [ "$deshablita_php" == s ] ; then
+
+	ht_file="wp-content/uploads/.htaccess"
+	if [ ! -f "$ht_file" ]
+
+		echo "deshabilitando..."
+		echo "
+<Files *.php>
+deny from all
+</Files>" >> wp-content/uploads/.htaccess
+		echo "Ok, archivos PHP deshabilitados."
+		then
+		    echo "${red} $0: El archivo'${ht_file}' ya existe, no se hicieron cambios.${clear}"
+		fi
+	
+	else
+		echo "Ok, saltando archivos PHP deshabilitados."
+	fi
+	
+read -p "${yellow}¿Deseas bloquear los pingbacks? [s/n]: ${clear}" bloquear_pingbacks
 if [ "$bloquear_pingbacks" == s ] ; then
+
 	echo "Bloqueando..."
 	echo "
 <Files xmlrpc.php>
@@ -151,6 +213,28 @@ if [ "$bloquear_pingbacks" == s ] ; then
 	echo "Ok, bloqueo de pingbacks realizado."
 else
 	echo "Ok, saltando bloqueo de pingbacks."
+fi
+
+read -p "${yellow}¿Bloquear navegación de carpetas? [s/n]: ${clear}" navegacion_carpetas
+if [ "$navegacion_carpetas" == s ] ; then
+	echo "Bloqueando..."
+	echo "Options -Indexes" >> .htaccess
+	echo "Ok, bloqueo de navegación de carpetas realizado."
+else
+	echo "Ok, saltando bloqueo de navegación de carpetas."
+fi
+
+read -p "${yellow}¿Deseas bloquear el acceso al .htaccess? [s/n]: ${clear}" bloquear_htaccess
+if [ "$bloquear_htaccess" == s ] ; then
+	echo "Bloqueando..."
+	echo "
+<Files .htaccess>
+order allow,deny
+deny from all
+</Files>" >> .htaccess
+	echo "Ok, bloqueo del .htaccess realizado."
+else
+	echo "Ok, saltando bloqueo del .htaccess."
 fi
 
 echo "Protegiendo wp-config.php contra escritura..."
